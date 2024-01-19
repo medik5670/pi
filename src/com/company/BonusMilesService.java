eaa0d819fc5749848dadf6e6624525c96bec5d76
Naty test <nastya-medyantsevaa34@mail.ru>
2024-01-19 12:46:37 +0300

    .gitignore

---
+++ src/com/company/BonusMilesService.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class BonusMilesService {
+    public static int calculate(int price) {
+        int ruble = 20;
+        int miles1 = 1;
+        int miles;
+        return miles = price * miles1 / ruble;
+    }
+}
